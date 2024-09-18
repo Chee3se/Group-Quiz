69b594af77818591f70b23adbc347d0434ab223b
MarkussIPA22 <markusspetrovs0@gmail.com>
2024-09-18 11:22:16 +0300

    models

--- database/migrations/2024_09_18_075553_create_answers_table.php
+++ database/migrations/2024_09_18_075553_create_answers_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('quizzes', function (Blueprint $table) {
+        Schema::create('answers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('title');
-            $table->text('description');
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-
-
+            $table->text('content');
+            $table->foreignId('question_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_correct');
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('quizzes');
+        Schema::dropIfExists('answers');
     }
 };
