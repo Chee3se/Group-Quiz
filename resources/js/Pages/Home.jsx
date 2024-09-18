564729096f102faf86da005a91ea333e6599f220
Chee3se <emilspete999@gmail.com>
2024-09-18 10:42:27 +0300

    gradient

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
             <Head title="Home"/>
 
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-                <h1 className="pt-48 mx-auto text-8xl font-bold">Quizy</h1>
+                <h1 className="pb-10 pt-48 mx-auto text-9xl font-extrabold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">Quizy</h1>
             </div>
         </Layout>
     );
