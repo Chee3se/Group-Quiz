bb16ca7b5498a0a6130ec7562060d2a13d690617
KandzaP <kandzapure@gmail.com>
2024-09-19 14:37:07 +0300

    quiz page shi

--- resources/js/Pages/Quiz/Index.jsx
+++ resources/js/Pages/Quiz/Index.jsx
@@ -1,5 +1,6 @@
 import { Link, Head } from '@inertiajs/react';
 import Layout from "@/Layouts/Layout.jsx";
+import QuestionLayout from './QuestionLayout';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     const handleImageError = () => {
@@ -13,9 +14,13 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
         <Layout>
             <Head title="quizes"/>
 
+            <QuestionLayout>
+            <Head title="Home"/>
+
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-                <h1 className="pb-10 pt-48 mx-auto text-9xl font-extrabold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">quiz page skr</h1>
+                <h1 className="pb-10 pt-48 mx-auto text-9xl font-extrabold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">Quizy</h1>
             </div>
+            </QuestionLayout>
         </Layout>
     );
 }
