6eface67b4eb74d3df74239936ff18f383c654da
KandzaP <kandzapure@gmail.com>
2024-09-19 14:07:46 +0300

    merged shi

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -34,7 +34,7 @@ export default function Layout({ children }) {
                                 Logout
                                 <span className={`absolute left-0 bottom-0 w-full h-0.5 ${url === '/logout' ? 'bg-gray-800' : 'bg-gray-800 transform scale-x-0'} group-hover:scale-x-100 transition-transform origin-bottom-right`}></span>
                             </Link>
-                        ) : (
+) : (
                             <>
                                 <Link href="/login" className="text-gray-800 relative group hover:text-gray-600">
                                     <span className="font-bold">
@@ -61,7 +61,7 @@ export default function Layout({ children }) {
         <path fill="#ffffff" fillOpacity="0.4" d="M0,192L48,186.7C96,181,192,171,288,160C384,149,480,139,576,144C672,149,768,171,864,165.3C960,160,1056,128,1152,128C1248,128,1344,160,1392,176L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         {/* <!-- Third wave layer --> */}
         <path fill="#ffffff" fillOpacity="0.2" d="M0,256L48,245.3C96,235,192,213,288,218.7C384,224,480,256,576,240C672,224,768,160,864,149.3C960,139,1056,181,1152,192C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-    </svg>dd
+    </svg>
 </div>
         </div>
     );
