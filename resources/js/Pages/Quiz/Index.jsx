cb76015e529f363e8d08af62808ad7da7be6d3ff
Chee3se <emilspete999@gmail.com>
2024-09-29 21:31:52 +0300

    Style

--- resources/js/Pages/Quiz/Index.jsx
+++ resources/js/Pages/Quiz/Index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Head, Link, router } from '@inertiajs/react'; // Import router for navigation
+import { Head, Link, router } from '@inertiajs/react';
 import Layout from "@/Layouts/Layout.jsx";
 import Waves from "@/Components/Waves.jsx";
 
@@ -27,10 +27,6 @@ export default function Index({ auth, quizzes }) {
         toggleScores(quizId);
     };
 
-    const handleQuizClick = (quizId) => {
-        router.get(`/quizzes/${quizId}`); // Navigate to the quiz page
-    };
-
     return (
         <Layout auth={auth}>
             <Head title="Quizzes" />
@@ -43,40 +39,59 @@ export default function Index({ auth, quizzes }) {
                     {auth.role === 'admin' && (
                         <Link
                             href={route('quizzes.create')}
-                            className="absolute top-4 right-4 w-12 h-12 bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg flex items-center justify-center hover:bg-gradient-to-r hover:from-purple-600 hover:to-pink-600 transition duration-300 z-20"
-                            style={{ boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }} // Add shadow for more emphasis
+                            className="absolute top-4 right-4 bg-green-500 rounded-lg flex items-center justify-center hover:bg-green-600 transition duration-300"
                         >
-                            <span className="text-white text-4xl pb-1">+</span>
+                            <span className="text-white px-6 py-2">Create</span>
                         </Link>
                     )}
                     <div className="grid grid-cols-1 gap-6 mt-12">
-                        {quizzes.map((quiz, index) => (
-                            <>
-                                <Link key={quiz.id} href={route('quizzes.show', {"id": quiz.id})} className="bg-gray-100 border-2 border-gray-200 rounded-lg p-6 hover:bg-purple-400 hover:text-white transition duration-300 flex justify-between items-center">
-                                    <div className="flex-1 text-center">
-                                        <div className="text-xl font-semibold">{`${quiz.title}`}</div>
-                                        <div className="text-sm mb-4">{`Question Count: ${quiz.questions_count}`}</div>
-                                    </div>
-                                    {auth.role === 'admin' && (
-                                        <div className="flex flex-col space-y-2">
-                                            <Link href={route('quizzes.edit', {"id": quiz.id})} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 text-center">
-                                                Edit
-                                            </Link>
-                                            <button onClick={() => handleDelete(quiz.id)} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300 text-center">
-                                                Delete
-                                            </button>
+                        {quizzes.map((quiz) => (
+                            <div key={quiz.id} className="relative bg-gray-100 border-2 border-gray-200 rounded-lg p-6">
+                                <div className="flex flex-row">
+                                    <Link
+                                        href={route('quizzes.show', { "id": quiz.id })}
+                                        className="flex-grow hover:bg-purple-400 hover:text-white transition duration-300 flex justify-between items-center"
+                                    >
+                                        <div className="flex-1 text-center">
+                                            <div className="text-xl font-semibold">{quiz.title}</div>
+                                            <div className="text-sm mb-4">{`Question Count: ${quiz.questions_count}`}</div>
                                         </div>
-                                    )}
-                                </Link>
-                                {/* High Scores Button */}
-                                <button
-                                    onClick={() => handleHighScoresClick(quiz.id)}
-                                    className="mt-2 bg-blue-500 text-white rounded-lg p-2 hover:bg-blue-700 transition duration-300"
-                                >
-                                    High Scores
-                                </button>
+                                    </Link>
 
-                                {/* High Scores Dropdown */}
+                                    <div className="flex flex-col space-y-2">
+                                        {auth.role === 'admin' && (
+                                            <>
+                                                <Link
+                                                    href={route('quizzes.edit', { "id": quiz.id })}
+                                                    className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 text-center"
+                                                    onClick={(e) => e.stopPropagation()}  // Stop link navigation
+                                                >
+                                                    Edit
+                                                </Link>
+                                                <button
+                                                    onClick={(e) => {
+                                                        e.stopPropagation(); // Prevent navigation
+                                                        handleDelete(quiz.id);
+                                                    }}
+                                                    className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300 text-center"
+                                                >
+                                                    Delete
+                                                </button>
+                                            </>
+                                        )}
+                                        {/* High Scores button */}
+                                        <button
+                                            onClick={(e) => {
+                                                e.stopPropagation(); // Prevent navigation
+                                                handleHighScoresClick(quiz.id);
+                                            }}
+                                            className="mt-2 bg-yellow-500 text-white rounded-lg p-2 hover:bg-yellow-600 transition duration-300"
+                                        >
+                                            High Scores
+                                        </button>
+                                    </div>
+                                </div>
+                                {/* High Scores dropdown */}
                                 {expandedQuizId === quiz.id && highScores[quiz.id] && (
                                     <div className="mt-4 bg-gray-50 border-2 border-gray-200 rounded-lg p-6">
                                         <h3 className="text-lg font-semibold mb-4">High Scores</h3>
@@ -104,7 +119,7 @@ export default function Index({ auth, quizzes }) {
                                         </table>
                                     </div>
                                 )}
-                            </>
+                            </div>
                         ))}
                     </div>
                 </div>
