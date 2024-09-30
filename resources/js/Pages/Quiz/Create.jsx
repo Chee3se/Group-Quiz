42468bb81e694f540cf8f7b94c46f823b33e0fb8
rudoxe <ipa22.r.vikmanis@vtdt.edu.lv>
2024-09-30 11:27:09 +0300

    createjsx delete icon

--- resources/js/Pages/Quiz/Create.jsx
+++ resources/js/Pages/Quiz/Create.jsx
@@ -113,7 +113,7 @@ export default function Create({ auth }) {
                                         className="ml-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300"
                                         onClick={() => deleteQuestion(qIndex)}
                                     >
-                                        Delete
+                                        <img src="/delete.svg" alt="Delete" className="w-8 h-8 inline-block" />
                                     </button>
                                 </div>
                                 <div className="grid grid-cols-2 gap-6">
